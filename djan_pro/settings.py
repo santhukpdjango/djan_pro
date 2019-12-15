0f1802299b61f6d89773f8ac44ed2c222a228bd0
santhukpdjango <santhukp@gmail.com>
2019-12-15 01:34:37 +0100

    added the correct django added home htmq0000

--- djan_pro/settings.py
+++ djan_pro/settings.py
@@ -119,6 +119,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
